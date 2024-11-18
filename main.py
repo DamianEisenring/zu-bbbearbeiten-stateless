14d802e038724db639434413457ea390c261e286
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2024-11-18 14:03:20 +0100

    pytest

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
+from flask import Flask, Response, redirect, render_template, request, url_for
+
 import helper
-from flask import Flask, request, Response, render_template, redirect, url_for
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     items = helper.get_all()
-    return render_template('index.html', items=items)
+    return render_template("index.html", items=items)
 
 
-@app.route('/add', methods=["POST"])
+@app.route("/add", methods=["POST"])
 def add():
     text = request.form.get("text")
-    helper.add(text)
+    date = request.form.get("deadline")
+    helper.add(text, date)
     return redirect(url_for("index"))
 
 
-@app.route('/update/<int:index>')
+@app.route("/update/<int:index>")
 def update(index):
     helper.update(index)
     return redirect(url_for("index"))
