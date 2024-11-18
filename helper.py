9f949b4afe0316386a51a16d835f7fadc63f963c
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2024-11-18 15:34:08 +0100

    adfasdf

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import datetime
 import operator
 from dataclasses import dataclass
@@ -39,3 +40,46 @@ def get(index):
 
 def update(index):
     items[index].isCompleted = not items[index].isCompleted
+=======
+import datetime
+import operator
+from dataclasses import dataclass
+
+items = []
+
+
+@dataclass
+class Item:
+    text: str
+    date: datetime
+    isCompleted: bool = False
+
+
+def oneWeekFromToday():
+    today = datetime.datetime.now()
+    oneWeek = datetime.timedelta(weeks=1)
+    return today + oneWeek
+
+
+def add(text, date=None):
+    text = text.replace("b", "bbb").replace("B", "Bbb")
+
+    if date is None:
+        date = oneWeekFromToday()
+    else:
+        date = datetime.datetime.strptime(date, "%Y-%m-%d")
+    items.append(Item(text, date))
+    items.sort(key=operator.attrgetter("date"))
+
+
+def get_all():
+    return items
+
+
+def get(index):
+    return items[index]
+
+
+def update(index):
+    items[index].isCompleted = not items[index].isCompleted
+>>>>>>> 19cd73c0e2367a93281e23c5913cd491b9d037f7
